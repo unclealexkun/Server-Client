05bc64bc344d0ed6ca1a814cf5a3ef7865a4d29d
UncleAlexKun <raisin27@mail.ru>
2023-08-17 16:01:20 +0400

    Добавил логирование

---
+++ ClientServer/Server/Controller/СalculatorController.cs
@@ -0,0 +1,24 @@
+﻿using EmbedIO;
+using EmbedIO.Routing;
+using EmbedIO.WebApi;
+
+namespace Server
+{
+	public class СalculatorController : WebApiController
+	{
+		#region Константы
+
+		/// <summary>
+		/// Логгер.
+		/// </summary>
+		private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
+		#endregion
+
+		[Route(HttpVerbs.Get, "/empty")]
+		public void GetEmpty()
+		{
+			Logger.Info("Nothing happened.");
+		}
+	}
+}
--- ClientServer/Server/Program.cs
+++ ClientServer/Server/Program.cs
@@ -18,6 +18,11 @@ namespace Server
 		/// </summary>
 		private readonly static Uri url = new Uri($"http://localhost:{port}/");
 
+		/// <summary>
+		/// Логгер.
+		/// </summary>
+		private static readonly NLog.Logger Logger = NLog.LogManager.GetCurrentClassLogger();
+
 		#endregion
 
 		#region Методы
@@ -27,20 +32,28 @@ namespace Server
 		/// </summary>
 		/// <param name="url">Адрес сервера.</param>
 		/// <returns>Веб-сервер.</returns>
-		private static WebServer CreateWebServer(string url)
+		private static WebServer? CreateWebServer(string url)
 		{
-			var server = new WebServer(o => o
+			try
+			{
+				var server = new WebServer(o => o
 							.WithUrlPrefix(url)
 							.WithMode(HttpListenerMode.EmbedIO))
 					.WithLocalSessionManager()
 					.WithWebApi("/api", m => m
-							.WithController<PeopleController>())
+							.WithController<СalculatorController>())
 					.WithModule(new ActionModule("/", HttpVerbs.Any, ctx => ctx.SendDataAsync(new { Message = "Error" })));
 
-			// Слушаем изменение состояния.
-			server.StateChanged += (s, e) => $"WebServer New State - {e.NewState}".Info();
+				// Слушаем изменение состояния.
+				server.StateChanged += (s, e) => $"WebServer New State - {e.NewState}".Info();
 
-			return server;
+				return server;
+			}
+			catch (Exception ex)
+			{
+				Logger.Error(ex.Message);
+				return null;
+			}
 		}
 
 		#endregion
@@ -49,11 +62,20 @@ namespace Server
 		{
 			using (var server = CreateWebServer(url.AbsoluteUri))
 			{
-				server.RunAsync();
-				// Подождите, пока будет нажата любая клавиша, прежде чем удалять наш веб-сервер.
-				// В сервисе мы будем управлять жизненным циклом нашего веб-сервера, используя
-				// что-то вроде BackgroundWorker или ManualResetEvent.
-				Console.ReadKey(true);
+				if (server != null)
+				{
+					server.RunAsync();
+					Logger.Trace("The server has been started...");
+					// Подождите, пока будет нажата любая клавиша, прежде чем удалять наш веб-сервер.
+					// В сервисе мы будем управлять жизненным циклом нашего веб-сервера, используя
+					// что-то вроде BackgroundWorker или ManualResetEvent.
+					Console.ReadKey(true);
+					Logger.Trace("The server has been stopped.");
+				}
+				else
+				{
+					Logger.Trace("The server has not been created.");
+				}	
 			}
 		}
 	}
